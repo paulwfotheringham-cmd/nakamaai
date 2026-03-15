a9d7ee0c9a51049a77be9dfca9e4217a77b9ec5b
paulwfotheringham-cmd <paul.w.fotheringham@gmail.com>
2026-03-15 22:32:09 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,23 +16,10 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Welcome to Nakama AI
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+         <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+AI-generated personalized audio stories created just for you.
+Choose your preferences, generate custom narratives, and listen instantly.
+</p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
